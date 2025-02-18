c704ce482a44beef260d037a96e85f5415ae400d
larpoux <larpoux@gmail.com>
2025-02-18 18:23:58 +0100

    doc

--- bin/live.sh
+++ bin/live.sh
@@ -3,14 +3,12 @@
 
 
 
-#rm -rf tau/fs/* tau/etau/* tau/taudio/*
-#cp -r ../etau/example/build/web tau/etau/live
-#cp -r ../flutter_sound/example/build/web tau/fs/live
-#cp -r ../taudio/example/build/web tau/taudio/live
-#if [ $? -ne 0 ]; then
-#    echo "Error"
-#    exit -1
-#fi
+rm -rf live
+cp -r ../flutter_sound/example/build/web live
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit -1
+fi
 
 
 exit 0
